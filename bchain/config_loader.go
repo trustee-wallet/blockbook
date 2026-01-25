9b6f4e9ea63a3cf54740e5c90df9d9a393a1380a
pragmaxim <pragmaxim@gmail.com>
2026-01-25 11:12:10 +0100

    utxo reorg detection fix

--- bchain/config_loader.go
+++ bchain/config_loader.go
@@ -17,8 +17,12 @@ import (
 // BlockchainCfg contains fields read from blockbook's blockchaincfg.json after being rendered from templates.
 type BlockchainCfg struct {
 	// more fields can be added later as needed
-	RpcUrl   string `json:"rpc_url"`
-	RpcUrlWs string `json:"rpc_url_ws"`
+	RpcUrl     string `json:"rpc_url"`
+	RpcUrlWs   string `json:"rpc_url_ws"`
+	RpcUser    string `json:"rpc_user"`
+	RpcPass    string `json:"rpc_pass"`
+	RpcTimeout int    `json:"rpc_timeout"`
+	Parse      bool   `json:"parse"`
 }
 
 // LoadBlockchainCfg returns the resolved blockchaincfg.json (env overrides are honored in tests)
