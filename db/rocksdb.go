13ceaa0cd6897f0e2165ffbe0d3c0624f576abd3
pragmaxim <pragmaxim@gmail.com>
2026-02-07 07:50:01 +0100

    lower addrContractsCacheMinSize

--- db/rocksdb.go
+++ db/rocksdb.go
@@ -57,7 +57,7 @@ const (
 	addressBalanceDetailUTXOIndexed = 2
 )
 
-const addrContractsCacheMinSize = 300_000 // limit for caching address contracts in memory to speed up indexing
+const addrContractsCacheMinSize = 100_000 // limit for caching address contracts in memory to speed up indexing
 
 // RocksDB handle
 type RocksDB struct {
